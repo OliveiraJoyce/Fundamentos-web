fb56d8254fd477ecbd93203b5be46591d3db6913
OliveiraJoyce <joycezer5@gmail.com>
2021-08-19 12:14:31 -0300

    Inserindo limite caracteres assunto, zoom maps e envio button do formulário

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -8,6 +8,10 @@ por nome: document.getElementsByName('email')
 let nome = window.document.getElementById("nome") /* Pegue o elemento pelo ID */
 let email = document.querySelector("#email") /*query obrigatório # */
 let assunto = document.querySelector("#assunto")
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector("#mapa")
 
 nome.style.width = "100%"
 email.style.width = "100%"
@@ -21,6 +25,7 @@ function validaNome() {
   } else {
     txtNome.innerHTML = "Nome Válido"
     txtNome.style.color = "green"
+    nomeOk = true
   }
 
 }
@@ -34,8 +39,47 @@ function validaEmail() {
   } else {
     txtEmail.innerHTML = "E-mail Válido"
     txtEmail.style.color = "green"
+    emailOk = true
   }
 }
+
+function validaAssunto() {
+  let txtAssunto = document.querySelector("#txtAssunto")
+
+  if (assunto.value.length >= 100) {
+    txtAssunto.innerHTML = "Texto é muito grande, digite no máximo 100 caracteres"
+    txtAssunto.style.color = "red"
+    txtAssunto.style.display = "block"
+
+  } else {
+    txtAssunto.style.display = "none"
+    assuntoOk = true
+
+  }
+}
+
+function enviar() {
+  if(nomeOk == true && emailOk == true && assuntoOk == true){
+    alert ("Formulário enviado com sucesso!")
+  } else { 
+    alert ("Preencha o formulário corretamente antes de enviar...")
+  }
+}
+
+function mapaZoom() {
+  mapa.style.width = "800px"
+  mapa.style.height = "600px"
+}
+
+function mapaNormal() {
+  mapa.style.width = "400px"
+  mapa.style.height = "250px"
+
+}
+
+
+
+
 
 
 
