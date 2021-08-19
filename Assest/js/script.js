6897405b53c9cc22c11de7b0ec0e80931b074e9a
OliveiraJoyce <joycezer5@gmail.com>
2021-08-18 21:06:05 -0300

    códigos JavaScript, utilizando os conceitos de DOM, enventos e funções

--- Assest/js/script.js
+++ Assest/js/script.js
@@ -5,33 +5,40 @@ por nome: document.getElementsByName('email')
  por ID: document.getElementById('#idDoelemento')
   por seletor: document.querySelector('id/classe/nome') */
 
-  let nome = window.document.getElementById("nome") /* Pegue o elemento pelo ID */
-  let email = document.querySelector("#email") /*query obrigatório # */
-  let assunto = document.querySelector("#assunto")
+let nome = window.document.getElementById("nome") /* Pegue o elemento pelo ID */
+let email = document.querySelector("#email") /*query obrigatório # */
+let assunto = document.querySelector("#assunto")
 
-  nome.style.width = "100%"
-  email.style.width = "100%"
-  assunto.style.width = "100%"
+nome.style.width = "100%"
+email.style.width = "100%"
+assunto.style.width = "100%"
 
-  function validaNome() {
-    let txtNome = document.querySelector("#txtNome")
-    if(nome.value.length < 3){
-      txtNome.innerHTML = "Nome Inválido"
-      txtNome.style.color = "red"
-    }else {
-      txtNome.innerHTML = "Nome Válido"
-      txtNome.style.color = "green"
-    }
-
+function validaNome() {
+  let txtNome = document.querySelector("#txtNome")
+  if (nome.value.length < 3) {
+    txtNome.innerHTML = "Nome Inválido"
+    txtNome.style.color = "red"
+  } else {
+    txtNome.innerHTML = "Nome Válido"
+    txtNome.style.color = "green"
   }
 
-  function validaEmail() {
-    let txtEmail = document.querySelector("#txtEmail")
+}
 
-    if(txtEmail.value.indexof("@")== -1|| email.value.indexof(".")){
-      txtEmail.innerHTML = "E-mail inválido"
-      txtEmail.style.color = "red"
-    }else {
-      txtEmail.innerHTML = "E-mail Válido"
-      txtEmail.style.color = "green"
+function validaEmail() {
+  let txtEmail = document.querySelector("#txtEmail")
+
+  if (email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+    txtEmail.innerHTML = "E-mail inválido"
+    txtEmail.style.color = "red"
+  } else {
+    txtEmail.innerHTML = "E-mail Válido"
+    txtEmail.style.color = "green"
   }
+}
+
+
+
+
+
+
